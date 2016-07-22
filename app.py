07555b63146097f66b8f7feb3ea22aade3fdffda
zvovov <zvovov@gmail.com>
2016-07-22 06:48:38 +0530

    mail working

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import json
 import sys
 import smtplib
 
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import requests
 from bs4 import BeautifulSoup
@@ -69,10 +69,10 @@ def getLatestResult(_only_last=False):
     """
     soup = getSoup(base_url)
     result_table = soup.find('table').find('tbody')
-    
+
     latest_date = result_table.find('tr').find_all('td')[-1].string
     latest_text = result_table.find('tr').find('td').find('a').string
-    
+
     if _only_last:
         return latest_text
 
@@ -89,7 +89,7 @@ def getLatestResult(_only_last=False):
 
 def checkUpdate():
     """
-    Returns True if result updated. False otherwise. 
+    Returns True if result updated. False otherwise.
     """
     if getLatestResult(_only_last=True) == current_latest:
         return False
@@ -98,24 +98,84 @@ def checkUpdate():
 
 def sendMail(_content):
     """
-    Sends email to the recepients with the _content 
+    Sends email to the recepients with the _content
     """
     print("MAIL!")
-    fromaddr = "YOUR ADDRESS"
-    toaddr = "ADDRESS YOU WANT TO SEND TO"
+
+    date = _content[0]
+    text = _content[1]
+
+    result="""\
+<html>
+<body>
+<h3>IPU Result - Latest</h3>
+<table>
+  <tr>
+    <th>Date</th>
+    <th>Result</th>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+  <tr>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+
+</table>
+<hr>
+<p><a href="http://www.ipu.ac.in/exam_results.php">DOWNLOAD RESULT</a></p>
+</body>
+</html>
+""".format(date[0], text[0], date[1], text[1], date[2], text[2], date[3], text[3],
+           date[4], text[4], date[5], text[5], date[6], text[6], date[7], text[7],
+           date[8], text[8], date[9], text[9])
+
+    from_addr = ""
+    to_addr = ""
     msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "SUBJECT OF THE MAIL"
-     
-    body = "YOUR MESSAGE HERE"
-    msg.attach(MIMEText(body, 'plain'))
-     
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = "TESTING"
+    msg.attach(MIMEText(result, 'html'))
+
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(fromaddr, "YOUR PASSWORD")
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    server.login(from_addr, "")
+    server.sendmail(from_addr, to_addr, msg.as_string())
     server.quit()
 
 
